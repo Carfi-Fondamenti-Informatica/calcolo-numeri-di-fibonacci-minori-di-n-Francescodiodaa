a1fe54cf449a92b4e913d25f76f65a0801cac534
Francescodiodaa <91884945+Francescodiodaa@users.noreply.github.com>
2021-11-11 09:59:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
-   return 0;
-}
+   int n_1=1, n_2=1,n,prox;
+   cin>>n;
+   if (n>=2){
+   for(int i=1; n_2<=n; i++) {
+       cout << n_2<<endl;
+       prox = n_1 + n_2;
+       n_2 = n_1;
+       n_1 = prox;
+   }}
+   else{
+       cout<<"errore"; }
+    return 0;
+   }
+
+
+
+
