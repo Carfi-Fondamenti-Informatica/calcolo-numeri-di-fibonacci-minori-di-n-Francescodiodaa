9b3fec8c1d027dd2bac1fddc7d23e951e192fdf1
Francescodiodaa <91884945+Francescodiodaa@users.noreply.github.com>
2021-11-11 10:02:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,12 @@ int main() {
    int n_1=1, n_2=1,n,prox;
    cin>>n;
    for(int i=1; n_2<=n; i++) {
-       cout << n_2<<endl;
+       cout << n_2 << endl;
        prox = n_1 + n_2;
        n_2 = n_1;
        n_1 = prox;
-
-    return 0;
-   }}
-
+   }
+    return 0;}
 
 
 
